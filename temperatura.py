8d63bed92628a1e167b52007bd61b46c7ff85eb3
David Bermúdez <78182257+DavidBmdz@users.noreply.github.com>
2026-08-18 18:10:33 -0600

    Update temperatura.py

--- temperatura.py
+++ temperatura.py
@@ -1,5 +1,4 @@
-#Solicita una temperatura en grados Celsius y calcula Fahrenheit mediante F = (C * 9 / 5) + 32.
-
+#Autor: David Bermúdez
 celsius = float(input("Ingrese la temperatura en grados Celsius: "))
 fahrenheit = celsius * 9 / 5 + 32
 print("La temperatura en Fahrenheit es: ", fahrenheit, "°F")
